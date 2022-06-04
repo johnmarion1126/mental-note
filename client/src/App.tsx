2ba6dfb3100bda7b4e6cbf72e3604d395539ce8a
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-04 14:07:54 -0700

    Install chakra-ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Center, ChakraProvider } from '@chakra-ui/react';
 
-const App = () => (
-  <div>
-    Hello World
-  </div>
+import theme from './theme';
+import Title from './components/Title';
+
+const App: React.FC = () => (
+  <ChakraProvider resetCSS theme={theme}>
+    <Center h="calc(100vh)">
+      <Title />
+    </Center>
+  </ChakraProvider>
 );
 
 export default App;
---
+++ client/src/components/Title.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+const Title: React.FC = () => (
+  <Box>
+    <h1>
+      Mental Note
+    </h1>
+  </Box>
+);
+
+export default Title;
