031465392e7886b316995ea1bfdac246c1f39fa8
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-03 18:31:49 -0700

    Setup graphql apollo typegraphql (#2)
    * Install dependencies
    
    * Set up apollo, type-graphql, and graphql

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,13 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-const constants_1 = require("./utils/constants");
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
 const dataSource_1 = __importDefault(require("./dataSource"));
-dotenv_1.default.config();
+const constants_1 = require("./utils/constants");
+const hello_1 = __importDefault(require("./resolvers/hello"));
 const main = async () => {
     await dataSource_1.default.initialize();
     const app = (0, express_1.default)();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [hello_1.default],
+        }),
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
     app.listen(constants_1.PORT, () => {
         console.log(`server started on localhost:${constants_1.PORT}`);
     });
