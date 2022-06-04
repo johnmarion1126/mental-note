c6331575a94768423550be5d61ac297dca9eb58e
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-04 14:41:54 -0700

    Create name input

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Center, ChakraProvider } from '@chakra-ui/react';
+import { Center, ChakraProvider, Flex } from '@chakra-ui/react';
 import '@fontsource/open-sans';
 import '@fontsource/raleway';
 
 import theme from './theme';
 import Title from './components/Title';
+import NameInput from './components/NameInput';
 
 const App: React.FC = () => (
   <ChakraProvider resetCSS theme={theme}>
     <Center h="calc(100vh)" bg="fifth" m="auto">
-      <Title />
+      <Flex flexDir="column">
+        <Title />
+        <NameInput />
+      </Flex>
     </Center>
   </ChakraProvider>
 );
---
+++ client/src/components/NameInput.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Input } from '@chakra-ui/react';
+
+const NameInput: React.FC = () => (
+  <Input variant="flushed" px={4} />
+);
+
+export default NameInput;
