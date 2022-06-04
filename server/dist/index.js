b3ac31467018f6b3736d449a417bb040cd4806f4
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-03 17:58:07 -0700

    Setup typeorm (#1)
    * Install dependencies
    
    * Start server
    
    * Start express server
    
    * Set up typeorm connection

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,2 +1,22 @@
-console.log("Hello World");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const constants_1 = require("./utils/constants");
+const dataSource_1 = __importDefault(require("./dataSource"));
+dotenv_1.default.config();
+const main = async () => {
+    await dataSource_1.default.initialize();
+    const app = (0, express_1.default)();
+    app.listen(constants_1.PORT, () => {
+        console.log(`server started on localhost:${constants_1.PORT}`);
+    });
+};
+main().catch((e) => {
+    console.error(e);
+});
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello World");
+import 'reflect-metadata';
+import express from 'express';
+import dotenv from 'dotenv';
+import { PORT } from './utils/constants';
+import dataSource from './dataSource';
+
+dotenv.config();
+
+const main = async () => {
+  await dataSource.initialize();
+  const app = express();
+
+  app.listen(PORT, () => {
+    console.log(`server started on localhost:${PORT}`);
+  });
+};
+
+main().catch((e) => {
+  console.error(e);
+});
