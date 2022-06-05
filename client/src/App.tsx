fa92ea13f190d0e79d0f0d41c24b760f00fe864b
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-04 17:19:26 -0700

    Add input validation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Center, ChakraProvider, Flex } from '@chakra-ui/react';
 import '@fontsource/open-sans';
 import '@fontsource/raleway';
@@ -7,15 +7,26 @@ import theme from './theme';
 import Title from './components/Title';
 import NameInput from './components/NameInput';
 
-const App: React.FC = () => (
-  <ChakraProvider resetCSS theme={theme}>
-    <Center h="calc(100vh)" bg="fifth" m="auto">
-      <Flex flexDir="column">
-        <Title />
-        <NameInput />
-      </Flex>
-    </Center>
-  </ChakraProvider>
-);
+const App: React.FC = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Center h="calc(100vh)" bg="fifth" m="auto">
+        {
+          !isSignedIn ? (
+            <Flex flexDir="column">
+              <Title />
+              <NameInput />
+            </Flex>
+          ) : (
+            <div>Signed In</div>
+          )
+        }
+      </Center>
+    </ChakraProvider>
+  );
+};
 
 export default App;
--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -2,7 +2,7 @@ import { Heading } from '@chakra-ui/react';
 import React from 'react';
 
 const Title: React.FC = () => (
-  <Heading fontSize="5xl" color="first" fontWeight="bold">
+  <Heading fontSize="5xl" color="second" fontWeight="bold">
     Mental Note
   </Heading>
 );
