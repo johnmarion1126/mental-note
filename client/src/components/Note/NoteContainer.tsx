e63195e1d75fa3438babcdc2efd52b65c75cf14f
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-04 17:45:53 -0700

    Create note container

--- client/src/components/Note/NoteContainer.tsx
+++ client/src/components/Note/NoteContainer.tsx
@@ -1,7 +1,16 @@
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 
 const NoteContainer: React.FC = () => (
-  <div>NoteContainer</div>
+  <Box
+    bg="white"
+    w="calc(55vh)"
+    h="calc(65vh)"
+    dropShadow="outline"
+    style={{
+      filter: 'drop-shadow(0px 0px 20px rgba(0,0,0,0.25))',
+    }}
+  />
 );
 
 export default NoteContainer;
---
+++ client/src/components/Note/NoteForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NoteForm: React.FC = () => (
+  <div>NoteForm</div>
+);
+
+export default NoteForm;
