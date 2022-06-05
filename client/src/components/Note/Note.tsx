01d184bbe822b1450a0654a1f4ec3d7d626b8235
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-04 18:03:33 -0700

    Create styling for note

--- client/src/components/Note/Note.tsx
+++ client/src/components/Note/Note.tsx
@@ -1,7 +1,30 @@
+import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const Note: React.FC = () => (
-  <div>Note</div>
+  <Box>
+    <Text color="gray.600">Dear Person</Text>
+    <Text
+      my={3}
+      textAlign="justify"
+      style={{
+        overflowX: 'hidden',
+        overflow: 'scroll',
+      }}
+      h="200px"
+      color="gray.600"
+    >
+      Aut officia est id quia.
+      Facilis velit rem doloribus
+      quisquam voluptatem. Animi
+      ducimus aut nesciunt. Quae
+      nam provident iusto consequatur
+      unde accusamus facilis facere.
+      Quidem deserunt inventore ut laboriosam.…
+    </Text>
+    <Text color="gray.600" textAlign="right">From,</Text>
+    <Text color="gray.600" textAlign="right">John</Text>
+  </Box>
 );
 
 export default Note;
--- client/src/components/Note/NoteContainer.tsx
+++ client/src/components/Note/NoteContainer.tsx
@@ -1,16 +1,22 @@
-import { Box } from '@chakra-ui/react';
 import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+import Note from './Note';
 
 const NoteContainer: React.FC = () => (
   <Box
     bg="white"
     w="calc(55vh)"
-    h="calc(65vh)"
+    h="calc(50vh)"
     dropShadow="outline"
     style={{
       filter: 'drop-shadow(0px 0px 20px rgba(0,0,0,0.25))',
     }}
-  />
+    px={8}
+    py={6}
+  >
+    <Note />
+  </Box>
 );
 
 export default NoteContainer;
